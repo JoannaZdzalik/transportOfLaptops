b0a688894e694cb7fe5984918ca3c3dacf483ff1
Joanna Zdzalik <asia.zdzalik@gmail.com>
2019-12-29 19:39:07 +0100

    "test"

--- src/main/java/recruitmentTask/Main.java
+++ src/main/java/recruitmentTask/Main.java
@@ -6,10 +6,10 @@ import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args ) throws IOException {
+    public static void main(String[] args, String aaaaa) throws IOException {
 
         CourseSolver s = new CourseSolver();
         s.solveCourses();
-
+        System.out.println(aaaaa);
     }
 }
